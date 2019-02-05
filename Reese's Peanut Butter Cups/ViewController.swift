ce85d9aeae5463c3164bd7d4927464451db3c7be
Anna Guo <annaguo03@gmail.com>
2019-02-04 20:21:27 -0500

    Reese's Peanut Butter Cups App: - Reese's Peanut Butter Cups phrase is toggled when Hey! button is pressed

--- Reese's Peanut Butter Cups/ViewController.swift
+++ Reese's Peanut Butter Cups/ViewController.swift
@@ -10,11 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var messageLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func heyButtonPressed(_ sender: UIButton) {
+        if messageLabel.text == "You got peanut butter in my chocolate!"
+        {messageLabel.textColor = UIColor.brown
+        messageLabel.textAlignment = NSTextAlignment.left
+        messageLabel.text = "You got chocolate in my peanut butter!"}
+        
+        else
+        { messageLabel.textColor = UIColor.orange
+            messageLabel.textAlignment = NSTextAlignment.right
+            messageLabel.text = "You got peanut butter in my chocolate!"}
+        
+    }
+    
 }
 
